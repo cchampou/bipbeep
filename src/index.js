706096fbcb71fc091a4ac3a9a419cfd4c43c7cfb
cchampou <clement@champouillon.com>
2020-05-06 01:07:46 +0200

    Adding favico

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => <h1>BipBeep</h1>;
+
+const rootDiv = document.getElementById('root');
+
+render(<App />, rootDiv);
+
